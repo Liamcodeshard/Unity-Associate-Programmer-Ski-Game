bfef09f2f2a7c9ca91990605e5df8d05f03c866b
Liamcodeshard <liam.ashton@hotmail.co.uk>
2023-01-12 11:45:15 +0000

    initial commit

---
+++ Unity Associate Programmer Ski Game/Assets/Scripts/CameraSwitcher.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class CameraSwitcher : MonoBehaviour
+{
+
+    [SerializeField] private List<CinemachineVirtualCamera> gameCameras = new List<CinemachineVirtualCamera>();
+    public static int camChoice = 0;
+
+
+    private void OnEnable()
+    {
+        foreach (var cam in gameCameras)
+        {
+            MainCamSwitcher.Register(cam);
+
+        }
+
+        MainCamSwitcher.SwitchCamera(gameCameras[0]);
+
+    }
+    private void OnDisable()
+    {
+        foreach (var cam in gameCameras)
+        {
+            MainCamSwitcher.UnRegister(cam);
+
+        }
+
+
+
+    }
+    void Update()
+    {
+        if (camChoice >= gameCameras.Count) camChoice = 0;
+        MainCamSwitcher.SwitchCamera(gameCameras[camChoice]);
+        
+
+    }
+
+
+
+}
---
+++ Unity Associate Programmer Ski Game/Assets/Scripts/MainCamSwitcher.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.PackageManager;
+using UnityEngine;
+using Cinemachine;
+
+public static class MainCamSwitcher
+{
+    static List<CinemachineVirtualCamera> cameras = new List<CinemachineVirtualCamera>();
+    public static CinemachineVirtualCamera ActiveCamera = null;
+
+    public static bool IsActiveCamera(CinemachineVirtualCamera camera)
+    {
+        return camera == ActiveCamera;
+       
+    }
+    public static void SwitchCamera(CinemachineVirtualCamera camera)
+    {
+        camera.Priority = 10;
+        ActiveCamera = camera;
+        foreach (CinemachineVirtualCamera c in cameras)
+        {
+            if (c != camera)
+            {
+                c.Priority = 0;
+            }
+        }
+        Debug.Log(ActiveCamera + " isActive");
+
+    }
+    public static void Register(CinemachineVirtualCamera camera)
+    {
+        cameras.Add(camera);
+        Debug.Log(camera + "Registered");
+    }
+
+    public static void UnRegister(CinemachineVirtualCamera camera)
+    {
+        cameras.Remove(camera);
+        Debug.Log(camera + "UnRegistered");
+    }
+
+}
