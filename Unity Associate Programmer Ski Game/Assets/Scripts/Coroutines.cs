4897d3c37e90756dc764a6b62155dd4f1a631a85
Liamcodeshard <liam.ashton@hotmail.co.uk>
2023-01-13 18:21:08 +0000

    Started on movement

---
+++ Unity Associate Programmer Ski Game/Assets/Scripts/Coroutines.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coroutines : MonoBehaviour
+{
+    public AudioSource audio;
+
+    public GameObject cube;
+    float counter;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //StartCoroutine(PlayAnswer());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+
+
+    }
+
+
+    public void DelayedChangeAfterAudio()
+    {
+        audio.Play();
+        Debug.Log("Audio Played");
+
+        Invoke("PlayAnswer", audio.clip.length);
+
+    }
+
+
+    void PlayAnswer()
+    {
+
+        Debug.Log(audio.time + " seconds later");
+
+    }
+}
--- Unity Associate Programmer Ski Game/Assets/Scripts/PlayerController.cs
+++ Unity Associate Programmer Ski Game/Assets/Scripts/PlayerController.cs
@@ -7,15 +7,41 @@ using System.Collections.Generic;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private int speeed = 60;
+    private Rigidbody rb;
+    public float speedIncrease = 0;
+    public float speed = 9.81f;
+    private float rotationY;
+    public float rotationSpeed =20;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.forward * speeed * Time.deltaTime);
+        MoveForward();
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(-Vector3.up * rotationSpeed * Time.deltaTime);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(Vector3.up * rotationSpeed * Time.deltaTime);
+        }
+
+
+    }
+
+    void MoveForward()
+    {
+       // float move_x = Input.GetAxis("Horizontal");
+        float move_z = Input.GetAxis("Vertical");
+        Vector3 Up = new Vector3(0f, rb.velocity.y, 0f);
+
+        rb.velocity = (transform.forward * move_z).normalized * speed + Up;
+        Debug.Log("Velocity altered");
     }
 }
